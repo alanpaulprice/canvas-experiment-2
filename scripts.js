4a12e7e0dc26199e057c825b2c2c233f01f9a0e0
alanpaulprice <23743415+alanpaulprice@users.noreply.github.com>
2018-02-07 18:30:55 +0000

    added bouncing squares

--- scripts.js
+++ scripts.js
@@ -11,4 +11,45 @@ function setCanvasSize(){
 window.addEventListener('resize', setCanvasSize);
 setCanvasSize();
 
-// ===== SQUARES ===== \\
+// ===== CREATE RECTANGLES ===== \\
+
+const rectArray = [];
+
+for (let i = 0; i < 100; i++){
+  let rect = {};
+  rect.width = rect.height = rnd() * 20;
+  rect.x = rnd() * innerWidth - rect.width;
+  rect.y = rnd() * innerHeight - rect.height;
+  rect.dx = (rnd() - 0.5) * 5;
+  rect.dy = (rnd() - 0.5) * 5;
+  rectArray.push(rect);
+}
+
+function draw(){
+  c.clearRect(0, 0, innerWidth, innerHeight);
+  rectArray.forEach((r)=>{
+
+    // ===== DRAW RECT ===== \\
+    c.fillStyle = '#fff';
+    c.fill();
+    c.fillRect(r.x, r.y, r.width, r.height);
+
+    // ===== COLLISION ===== \\
+    if (r.x > innerWidth - r.width || r.x < 0){
+      r.dx = -r.dx;
+    }
+    if (r.y > innerHeight - r.height || r.y < 0){
+      r.dy = -r.dy;
+    }
+
+    // ===== VELOCITY ===== \\
+    r.x += r.dx;
+    r.y += r.dy;
+  });
+}
+
+function animate(){
+  draw();
+  requestAnimationFrame(animate);
+}
+requestAnimationFrame(animate);
